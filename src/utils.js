3b3afc3d53bcedd8025d1f4eb519dfc8c83d1d5a
vandeurenglenn <vandeurenglenn@gmail.com>
2021-08-15 16:33:37 +0200

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,38 @@
+export const networksById = {
+  1: 'mainnet',
+  3: 'ropsten',
+  5: 'goerli',
+  137: 'polygon-mainnet',
+  8001: 'polygon-mumbai',
+}
+
+export const networksByName = {
+  'mainnet': 1,
+  'ropsten': 3,
+  'goerli': 5,
+  'polygon-mainnet': 137,
+  'polygon-mumbai': 8001,
+}
+
+export const DEFAULT_NETWORK = {
+  name: 'mainnet',
+  chainId: 1,
+}
+
+export const networkFromRpc = (url) => {
+  const network = DEFAULT_NETWORK
+  for (const name of Object.keys(networksByName)) {
+    if (url.includes(name)) {
+      network.name = name
+      network.chainId = networksByName[name]
+    }
+  }
+  return network
+}
+
+export default {
+  DEFAULT_NETWORK,
+  networkFromRpc,
+  networksById,
+  networksByName,
+}
